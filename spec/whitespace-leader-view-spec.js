82f909a75a3f8d75c33494c402a65d2b82d4e05b
roscolil <ross.lillis@gmail.com>
2017-12-02 18:06:30 +1100

    initial commit

---
+++ spec/whitespace-leader-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WhitespaceLeaderView from '../lib/whitespace-leader-view';
+
+describe('WhitespaceLeaderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
